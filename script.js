c38c1023f4c345f3a106633ea517b63f6dcbab51
Abdul Ragib <abdulragib36@gmail.com>
2022-12-30 22:49:01 +0530

    till Z to A done

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function showData(data){
             const createDiv = document.createElement('tr')
             createDiv.innerHTML = ` 
             <td id="id">${item.id}</td>
-            <td colspan="3"><img src="${item.img_src}" width="20px" height="20px" id="photo"/> <div id="name">${item.first_name + item.last_name}</div></td>
+            <td colspan="3"><img src="${item.img_src}" width="20px" height="20px" id="photo"/> <div id="name">${item.first_name} ${item.last_name}</div></td>
             <td id="gender">${item.gender}</td>
             <td id="class">${item.class}</td>
             <td id="marks">${item.marks}</td>
@@ -38,3 +38,142 @@ function showData(data){
    )  
 })
 }
+
+
+// search box filter function
+function filterStudents(students, searchTerm, table) {
+    // Convert the search term to lower case for case-insensitive matching
+    if(searchTerm === undefined)
+    {
+        return
+    }
+
+    searchTerm = searchTerm.toLowerCase();
+  
+    // Filter the list of students based on the search term
+    const filteredStudents = Object.values(students).filter(student => {
+      return (
+        student.first_name.toLowerCase().includes(searchTerm) ||
+        student.last_name.toLowerCase().includes(searchTerm) ||
+        student.email.toLowerCase().includes(searchTerm)
+      );
+    });
+
+    console.log(filteredStudents)
+  
+    // Clear the table
+    table.innerHTML = "";
+  
+    // Populate the table with the filtered list of students
+    for (const student of filteredStudents) {
+      const row = document.createElement("tr");
+      row.innerHTML = ` 
+      <td id="id">${item.id}</td>
+      <td colspan="3"><img src="${item.img_src}" width="20px" height="20px" id="photo"/> <div id="name">${item.first_name} ${item.last_name}</div></td>
+      <td id="gender">${item.gender}</td>
+      <td id="class">${item.class}</td>
+      <td id="marks">${item.marks}</td>
+      <td id="passing">${pass}</td></td>
+      <td id="mail">${item.email}</td>`
+      table.appendChild(row);
+    }
+  }
+  
+
+  const students = calling('./MOCK_DATA.json')
+  const searchTerm = document.getElementById('input-box').value
+  const table = document.getElementById('show-table')
+  const searchButton = document.getElementById('search-button')
+  console.log("hello world"+searchTerm)
+
+  searchButton.onclick = filterStudents(students, searchTerm.value, table)
+
+
+
+// sort A -> Z
+
+const aToz = document.getElementById('one')
+aToz.addEventListener('click', () => {
+
+    const students = calling('./MOCK_DATA.json')
+    students.then((item) => {
+
+      const table = document.getElementById('show-table')
+      const sortedStudents = Object.values(item).sort((a, b) => {
+          return a.first_name.localeCompare(b.first_name);
+        });
+  
+        console.log(sortedStudents)
+      table.innerHTML = "";
+      for (const student of sortedStudents) {
+          const row = document.createElement("tr");
+  
+          var pass
+              if(student.passing)
+              {
+                 pass='passing'
+              }
+              else{
+                  pass='failing'
+              }
+  
+          row.innerHTML =` 
+          <td id="id">${student.id}</td>
+          <td colspan="3"><img src="${student.img_src}" width="20px" height="20px" id="photo"/> <div id="name">${student.first_name} ${student.last_name}</div></td>
+          <td id="gender">${student.gender}</td>
+          <td id="class">${student.class}</td>
+          <td id="marks">${student.marks}</td>
+          <td id="passing">${pass}</td></td>
+          <td id="mail">${student.email}</td>`
+          
+          table.appendChild(row);
+      }
+    })
+    
+})
+
+// sort z -> A
+
+const zToa = document.getElementById('two')
+zToa.addEventListener('click', () => {
+
+    const students = calling('./MOCK_DATA.json')
+    students.then((item) => {
+
+      const table = document.getElementById('show-table')
+      const sortedStudents = Object.values(item).sort((a, b) => {
+          return a.first_name.localeCompare(b.first_name);
+        });
+  
+        const reverse = sortedStudents.reverse()
+      table.innerHTML = "";
+      for (const student of reverse) {
+          const row = document.createElement("tr");
+  
+          var pass
+              if(student.passing)
+              {
+                 pass='passing'
+              }
+              else{
+                  pass='failing'
+              }
+  
+          row.innerHTML =` 
+          <td id="id">${student.id}</td>
+          <td colspan="3"><img src="${student.img_src}" width="20px" height="20px" id="photo"/> <div id="name">${student.first_name} ${student.last_name}</div></td>
+          <td id="gender">${student.gender}</td>
+          <td id="class">${student.class}</td>
+          <td id="marks">${student.marks}</td>
+          <td id="passing">${pass}</td></td>
+          <td id="mail">${student.email}</td>`
+          
+          table.appendChild(row);
+      }
+    })
+    
+})
+
+
+
+
